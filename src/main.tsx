6ea423df69ca773646a7a8dd207fd982ad4d7d44
Gabriel <gabriel@swispace.com>
2024-08-22 11:24:38 +0200

    chore: setting up posthog

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
+import posthog from "posthog-js";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./scss/root.scss";
+
+posthog.init("phc_BJg7ERvmod7hJSjby55IeT2yrwyWOetVQfOj10SYgdq", {
+  api_host: "https://eu.posthog.com",
+});
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById("root")!).render(
